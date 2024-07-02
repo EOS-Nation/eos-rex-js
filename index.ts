654087ef39806cc6609dd85863c40713372a683b
Denis Carriere <carriere.denis@gmail.com>
2024-07-02 12:20:29 +0200

    init rex repo

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { APIClient } from "@wharfkit/antelope"
+import * as System from "./codegen/eosio"
+
+export async function get_rexpool(client: APIClient) {
+    const response = await client.v1.chain.get_table_rows({code: "eosio", scope: "eosio", table: "rexpool"})
+    return System.Types.rex_pool.from(response.rows[0])
+}
+
+export async function get_rexretpool(client: APIClient) {
+    const response = await client.v1.chain.get_table_rows({code: "eosio", scope: "eosio", table: "rexretpool"})
+    return System.Types.rex_return_pool.from(response.rows[0])
+}
+
+export async function get_retbuckets(client: APIClient) {
+    const response = await client.v1.chain.get_table_rows({code: "eosio", scope: "eosio", table: "retbuckets"})
+    return System.Types.rex_return_buckets.from(response.rows[0])
+}
+
+export async function get_rex_apy(client: APIClient) {
+    const rexpool = await get_rexpool(client);
+    const rexretpool = await get_rexretpool(client);
+    const total_lendable = rexpool.total_lendable.units.toNumber();
+    const current_rate_of_increase = rexretpool.current_rate_of_increase.toNumber();
+    const proceeds = rexretpool.proceeds.toNumber();
+    return((proceeds + current_rate_of_increase) / 30 * 365) / total_lendable;
+}
--- test.ts
+++ test.ts
@@ -1,6 +1,3 @@
-# EOS REX Javascript
-
-```javascript
 import { APIClient } from "@wharfkit/antelope"
 import { Chains } from "@wharfkit/common";
 import { get_rex_apy } from ".";
@@ -8,4 +5,3 @@ import { get_rex_apy } from ".";
 const client = new APIClient({url: Chains.EOS.url})
 
 get_rex_apy(client).then(console.log).catch(console.error)
-```
